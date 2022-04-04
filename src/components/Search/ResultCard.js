b30fd12cf4e2dd403cbb232c7fe39c8da7cc7ee1
matucasal <matias.casal@yahoo.com.ar>
2022-04-03 21:26:30 -0300

    grid with results ready

--- src/components/Search/ResultCard.js
+++ src/components/Search/ResultCard.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import './ResultCard.scss'
+
+
+const truncate = (str, max, suffix) => str.length < max ? str : `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`;
 
 class ResultCard extends Component {
 
-    constructor(props) {
-        super(props);
-     
-    }
 
     render() {
 
-        const { result } = this.props;
+        console.log('this.props.thumbnail', this.props.thumbnail)
 
         return (
         
-            <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>{this.props.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Card.Text>
-                {this.props.description}
-                </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
+            <Card style={{ width: '25rem', height: '25rem' }} className="card">
+                <Card.Img variant="top" className="card-image" src={this.props.thumbnail}  />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Location: { this.props.location || '-'}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Photographer: {this.props.photographer || '-'}</Card.Subtitle>
+                    <Card.Text>
+                    {truncate(this.props.description, 100, '...')}
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
             </Card>
         
         );
--- src/components/Search/Results.js
+++ src/components/Search/Results.js
@@ -1,7 +1,21 @@
 import ResultCard from './ResultCard';
 import constant from '../../constants/moon.json'
 import React, { Component } from 'react';
-import CardGroup from 'react-bootstrap/CardGroup';
+import './Result.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const getPreviewFromLinks = (arrObj) => {
+    console.log('arrObj', arrObj)
+    if(arrObj){
+        const index = arrObj.map(object => object.rel).indexOf('preview');
+        console.log('arrObj[index]', arrObj[index])
+        return arrObj[index].href
+    }
+    
+}
+
 
 class Results extends Component {
 
@@ -18,18 +32,29 @@ class Results extends Component {
     }
 
     render() {
-
         const resultList = this.getResults()
-        const resultItems = resultList.map((result) =>
-            /*<li key={result.data[0].nasa_id}>{result.data[0].title}</li> */
-            <ResultCard key={result.data[0].nasa_id} title={result.data[0].title} description={result.data[0].description}/>
-        );
-
+        
         return (
-            <div>
-                <CardGroup>
-                    {resultItems}
-                </CardGroup>
+            <div className="search-results">
+                <Row xs={1}>
+                    {
+                        resultList.map((result) =>
+                            <Col key={result.data[0].nasa_id}  md="auto"> 
+                                <ResultCard 
+                                key={result.data[0].nasa_id} 
+                                title={result.data[0].title} 
+                                description={result.data[0].description}
+                                location={result.data[0].location}
+                                photographer={result.data[0].photographer}
+                                thumbnail={getPreviewFromLinks(result.links)}
+                                />
+                            </Col>
+                        )
+                    }
+                </Row>
+            
+               
+                
             </div>
         )
     }
